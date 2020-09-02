e9d8c647e48eec7851f7b3f5a8d2a98b0f041efb
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-09-02 23:12:44 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
+import React,{ Component } from "react";
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component{
+    focus(){
+        console.log("Good!")
+    }
+    blur(){
+        console.log("Hey! Eyes on me!");
+    }
+    render(){
+        return(
+            <button onFocus={this.focus} onBlur={this.blur}>Check Me</button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
+import React,{ Component } from "react";
+
 // Code Keypad Component Here
+export default class Keypad extends Component{
+
+    KeyUp(){
+        console.log("Entering password...");
+    }
+    render(){
+        return( 
+        <input type="password" onKeyUp={this.KeyUp}/>
+        )
+    }
+}
